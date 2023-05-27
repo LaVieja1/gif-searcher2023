fc1586db0bf4fcc24d84dc1d81b4da34ccb06abc
santicano <canosantiago404@gmail.com>
2023-05-26 23:30:49 -0300

    Terminado

---
+++ script.js
@@ -0,0 +1,18 @@
+const img = document.querySelector('img');
+const searchBox = document.querySelector('#search');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', searchGif);
+
+function searchGif() {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=33Ny2IXp2gAZ8HSlJNhMX5dQS9BIB77p&s=${searchBox.value}`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+    })
+    .catch((function() {
+        img.src = 'error.jpg';
+    }))
+};
