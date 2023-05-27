a5255bb5778364343c7b4cb6ac7cdae54bc7f05b
santicano <canosantiago404@gmail.com>
2023-05-27 11:27:38 -0300

    Ahora la funcion searchGif es asincronica con async/await try/catch

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', searchGif);
 
+/*
 function searchGif() {
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=33Ny2IXp2gAZ8HSlJNhMX5dQS9BIB77p&s=${searchBox.value}`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    })
-    .catch((function() {
-        img.src = 'error.jpg';
-    }))
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        })
+        .catch((function() {
+            img.src = 'error.jpg';
+        }))
 };
+*/
+
+async function searchGif() {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=33Ny2IXp2gAZ8HSlJNhMX5dQS9BIB77p&s=${searchBox.value}`, {mode: 'cors'})
+        const gifData = await response.json();
+        img.src = gifData.data.images.original.url;
+    } catch (error) {
+        img.src = 'error.jpg';
+    }
+}
